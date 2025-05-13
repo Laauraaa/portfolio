e910f4a574932ca3cc3b9cf747053d052c9f24e0
Laura <analaura3rocha@gmail.com>
2025-05-13 17:37:49 -0300

    Update About.tsx

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -18,7 +18,7 @@ export function About(){
             <div className={styles.text_area} >
 
                 <div>
-                    <img src="dist/icon-me.png" alt="" className={styles.icon_me}/>
+                    <img src="dist/img/icon-me.png" alt="" className={styles.icon_me}/>
                 </div>
 
                 <div className={styles.text}>
